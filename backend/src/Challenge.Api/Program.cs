fc50818e9131e4bb546f887213f1c400a9fe9003
Asef <asefhosseini00@gmail.com>
2026-09-14 17:55:10 +0330

    feat: configure JWT authorization in Swagger

--- backend/src/Challenge.Api/Program.cs
+++ backend/src/Challenge.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi;
 using Challenge.Api.Auth;
 using Challenge.Api.Data;
 using Challenge.Api.Middleware;
@@ -19,7 +20,22 @@ builder.Services.AddSingleton(
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+
+    options.AddSecurityRequirement(document =>
+        new OpenApiSecurityRequirement
+        {
+            [new OpenApiSecuritySchemeReference("bearer", document)] = []
+        });
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
